77c0e8fe35f3c4a3df0130bef0ec3ffa76f6d971
Slav Pisarov <slav.pisarov@gmail.com>
2024-01-24 17:49:13 +0200

    Add styling to keyboard

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,9 +1,14 @@
 import KEYS from "./Keys"
+import styles from "./Keyboard.module.css"
 
 export function Keyboard() {
     console.log(KEYS);
     
-    return <div>
-        
+    return <div className={styles.keyboard}>
+        {KEYS.map(key =>{
+            return(
+                <button key={key} className={`${styles.btn}`}>{key}</button>
+            )
+        })}
     </div>
 }
