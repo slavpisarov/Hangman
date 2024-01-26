d0bd4b4bb730f9927ebd05f6c03f1cec3e21b81a
Slav Pisarov <slav.pisarov@gmail.com>
2024-01-26 16:56:50 +0200

    move styles for word to a module.css

--- src/components/HangmanWord/HangmanWord.tsx
+++ src/components/HangmanWord/HangmanWord.tsx
@@ -1,24 +1,19 @@
+import styles from './HangmanWord.module.css'
+
 type HangmanWordProps ={
     guessedLetters: string[],
     wordToGuess: string,
     reveal?:boolean
 }
 
+
 export function HangmanWord({
     guessedLetters, wordToGuess, reveal=false}:HangmanWordProps) {
 
-    return <div
-             style={{
-                display:"flex",
-                gap:".25em",
-                fontSize:"6rem",
-                fontWeight:"bold",
-                textTransform:"uppercase",
-                fontFamily:"monospace"
-             }}>
+    return <div className={styles.word}>
 
                 {wordToGuess.split("").map((letter,index) =>(
-                    <span style={{borderBottom:".1em solid black"}} key={index}>
+                    <span className={styles.underline} key={index}>
                         <span 
                           style={{
                             visibility: guessedLetters.includes(letter) || reveal
