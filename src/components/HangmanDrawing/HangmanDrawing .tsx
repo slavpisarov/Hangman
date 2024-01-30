4323e34b79b5c808a243999695027bb14a8bd6f8
Slav Pisarov <slav.pisarov@gmail.com>
2024-01-30 17:19:54 +0200

    Move drawing css to a module

--- src/components/HangmanDrawing/HangmanDrawing .tsx
+++ src/components/HangmanDrawing/HangmanDrawing .tsx
@@ -1,4 +1,5 @@
 import BODY_PARTS from "./BodyParts"
+import styles from './HangmanDrawing.module.css'
 
 type HangmanDrawingProps = {
     numberOfGuesses: number
@@ -10,9 +11,9 @@ export function HangmanDrawing(
  
     return <div style={{position:"relative"}}>
         {BODY_PARTS.slice(0,numberOfGuesses)}
-        <div style={{height:"50px", width:"10px", background:"black",position:"absolute", top:0, right:0}}/>
-        <div style={{height:"10px", width:"200px", background:"black", marginLeft:"120px"}}/>
-        <div style={{height:"400px", width:"10px", background:"black", marginLeft:"120px"}}/>
-        <div style={{height:"10px", width:"250px", background:"black"}}/>
+        <div className={styles.rope}/>
+        <div className={styles.verticalLine}/>
+        <div className={styles.horizontalLine}/>
+        <div className={styles.base}/>
     </div>
 }
